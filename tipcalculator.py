38acee88a4b80b838b299329f8e84fd5d749c99d
nikhilsable <nikhilsable@gmail.com>
2016-10-16 11:35:49 -0400

    Add files via upload
    added code to round to the 2nd decimal place

--- tipcalculator.py
+++ tipcalculator.py
@@ -2,7 +2,7 @@
 
 def tipcalc(bill):
     tip = 0.15 * bill
-    print("Your tip should be: $" + str(tip))
+    print"Your tip should be: $" , ("%.2f" % tip)
     
 amount = float(raw_input("How much was your total bill? "))
 tipcalc(amount)
